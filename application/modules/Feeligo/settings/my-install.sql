371a40aee4893a514567fea9238d2b2025bfd51c
Daniel Ross <daniel.je.ross@gmail.com>
2013-02-12 17:12:07 +0100

    initial adding of send buttons

--- application/modules/Feeligo/settings/my-install.sql
+++ application/modules/Feeligo/settings/my-install.sql
@@ -32,3 +32,15 @@ CREATE TABLE IF NOT EXISTS `engine4_feeligo_gifts` (
 --
 
 INSERT IGNORE INTO `engine4_core_modules` (`name`, `title`, `description`, `version`, `enabled`, `type`) VALUES  ('feeligo', 'Feeligo', 'Monetize and Engage your community by allowing your members to send virtual gifts to each other!', '4.1.0.2.3', 1, 'extra') ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `engine4_core_menuitems`
+--
+
+INSERT IGNORE INTO `engine4_core_menuitems` (`name`, `module`, `label`, `plugin`, `params`, `menu`, `submenu`, `order`) VALUES
+('flg_send_gift_user_home', 'core', 'Send a gift', 'Feeligo', '{"uri":"#_flg-send-gift","icon":"","target":"","enabled":"1"}', 'user_home', '', 6),
+('flg_send_gift_user_profile', 'core', 'Send them gift', 'Feeligo', '{"uri":"#_flg-send-gift-to-subject","icon":"","target":"","enabled":"1"}', 'user_profile', '', 3)
+;
